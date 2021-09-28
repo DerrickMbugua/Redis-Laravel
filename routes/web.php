0f1ae93fa266555bf495e37e2f4c53e491ac5547
Derrick Mwema <derrickmbugua50@gmail.com>
2021-09-28 12:50:16 +0300

    feat: send email

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Mail\SendMail;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +17,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/send-email', function () {
+    Mail::to('derrickmbugua50@gmail.com')->send(new SendMail());
+    return "Email sent successfully";
 });
